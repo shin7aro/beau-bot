4d7a7a1ec346a5c06490cb0cf26b670204fa105a
shin7aro <40793919+shin7aro@users.noreply.github.com>
2026-08-22 22:32:48 +0300

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -173,7 +173,7 @@ const commands = [
     .addSubcommand((sub) =>
       sub
         .setName('mark-claimed')
-        .setDescription("Manually mark someone as having taken their share, e.g. if they forgot to react (organizer or server manager only)")
+        .setDescription("Mark someone as having taken their share (e.g. they forgot to react)")
         .addStringOption((opt) => opt.setName('split_id').setDescription('The split ID shown in the embed footer').setRequired(true))
         .addUserOption((opt) => opt.setName('member').setDescription('The participant who already took their share').setRequired(true))
     ),
